eafb308e76cc2a4f5100d7a4cb8c35c4168684bf
iceiony <iceiony@yahoo.com>
2017-05-19 15:26:22 +0100

    Update error collection for chainer

--- chainer/XOR.py
+++ chainer/XOR.py
@@ -1,59 +1,72 @@
+CUDA = True
+OUTPUT_FILE = 'xpu.csv'
+ITERATIONS = 500
+
+
 import matplotlib.pyplot as plt
-import numpy as np
-from chainer.cuda import cupy as cp
 import chainer
 from chainer import Chain, Variable, optimizers
 import chainer.functions as F
 import chainer.links as L
+import chainer.initializers as initializers
+
+if CUDA:
+    print('GPU run')
+    from chainer.cuda import cupy as xp
+else:
+    print('CPU run')
+    import numpy as xp
 
 class MLPXOR(Chain):
     def __init__(self):
         super(MLPXOR, self).__init__(
-            # the size of the inputs to each layer will be inferred
-            l1=L.Linear(None, 2),
-            l2=L.Linear(None, 1),
+            l1=L.Linear(2, 2, initialW=initializers.Normal(scale=1)),
+            l2=L.Linear(2, 1, initialW=initializers.Normal(scale=1)),
         )
 
     def __call__(self, x):
-        h1 = F.leaky_relu(self.l1(x))
-        return self.l2(h1)
+        h1 = F.sigmoid(self.l1(x))
+        return self.l2(h1) #linear output for sigmoid_cross_entropy()
 
-model = MLPXOR()
+def run_model():
+    model = MLPXOR()
 
-#chainer.cuda.get_device_from_id(0).use()
-#model.to_gpu()
-#np = cp
+    if CUDA:
+        chainer.cuda.get_device_from_id(0).use()
+        model.to_gpu()
 
-data_in = np.array([[0, 0],
-                    [0, 1],
-                    [1, 0],
-                    [1, 1]]).astype('f')
-data_out = np.array([0, 1, 1, 0]).astype('i').reshape((4, 1))
+    data_in = xp.array([[0, 0],
+                        [0, 1],
+                        [1, 0],
+                        [1, 1]]).astype('f')
+    data_out = xp.array([0, 1, 1, 0]).astype('i').reshape((4, 1))
+
+    optimizer = optimizers.SGD(1.0)
+    optimizer.setup(model)
+
+    x = Variable(data_in)
+    y = Variable(data_out)
+
+    errors = []
+    for it in range(1, ITERATIONS):
+        model.cleargrads()
+        loss = F.sigmoid_cross_entropy(model(x), y)
+        errors.append(loss.data)
+        loss.backward()
+        optimizer.update()
+    return errors
 
 
-optimizer = optimizers.SGD(1.0)
-optimizer.setup(model)
+f = open(OUTPUT_FILE, 'w')
+print('Train errors: ')
+for i in range(1):
+    errors = run_model()
+    print(', '.join(map(str,errors)), file=f)
+    print(errors[-1])
+f.close()
 
-x = Variable(data_in)
-y = Variable(data_out)
+if CUDA:
+    errors = [xp.asnumpy(x) for x in errors]
+plt.plot(errors)
+plt.savefig('error.png')
 
-errors = []
-for it in range(1, 5000):
-    model.cleargrads()
-    error = F.sigmoid_cross_entropy(model(x), y)
-    error.backward()
-    optimizer.update()
-    # errors.append(error.data)
-
-
-print('Train error ', error.data)
-# model = F.sigmoid(model(x))
-# print('Output :\n', np.round(cp.asnumpy(model.data)))
-# print('Intended :\n', y.data)
-# plt.plot(errors)
-# plt.show()
-#
-# import chainer.computational_graph as c
-# g = c.build_computational_graph(model)
-# with open('./XOR.DOT', 'w') as o:
-#    o.write(g.dump())
